af966614125836f87fcc29ff0ac9512f4e0ea191
hsjqismonster <72074964+hsjqismonster@users.noreply.github.com>
2022-07-23 16:31:19 +0800

    Delete compiler directory

--- compiler/tp6/app/Edubot/provider.php
+++
@@ -1,9 +0,0 @@
-<?php
-use app\Edubot\ExceptionHandle;
-use app\Edubot\Request;
-
-// 容器Provider定义文件
-return [
-    'think\Request'          => Request::class,
-    'think\exception\Handle' => ExceptionHandle::class,
-];
--- compiler/tp6/app/Index/Request.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace app\Index;
-
-// 应用请求对象类
-class Request extends \think\Request
-{
-
-}
