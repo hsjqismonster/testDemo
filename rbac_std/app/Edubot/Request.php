fbd29fd14b8c62fbf3ad770454e11141c1dd466d
hsjqismonster <72074964+hsjqismonster@users.noreply.github.com>
2022-07-23 16:31:31 +0800

    Delete rbac_std directory

--- rbac_std/app/Edubot/Request.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace app\Edubot;
-
-// 应用请求对象类
-class Request extends \think\Request
-{
-
-}
--- rbac_std/app/RbacStd/Request.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace app\RbacStd;
-
-// 应用请求对象类
-class Request extends \think\Request
-{
-
-}
